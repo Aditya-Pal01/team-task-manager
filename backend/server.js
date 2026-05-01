08d66cf67f323d63f03dadbb049b589cb1972b4a
Aditya Pal <2k22.csds.2212974@gmail.com>
2026-05-01 18:12:51 +0530

    fix cors + frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,17 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://considerate-purpose-production-2082.up.railway.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,38 +1,32 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api";
+import API from "../api";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
 
-  const login = async () => {
-    const res = await api.post("/auth/login", {
-      email,
-      password,
-    });
+  const handleLogin = async () => {
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
 
-    localStorage.setItem("token", res.data.token);
-    navigate("/dashboard");
+      localStorage.setItem("token", res.data.token); // 🔥 yahi main hai
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login failed");
+    }
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div>
       <h2>Login</h2>
-
-      <input
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        placeholder="Password"
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button onClick={login}>Login</button>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
