c21b38d53a6668211c138d9a6c5b2d6f5403ed58
Aditya Pal <2k22.csds.2212974@gmail.com>
2026-05-02 00:56:15 +0530

    dd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://considerate-purpose-production-2082.up.railway.app/api"
+  baseURL: "https://team-task-manager-production-f529.up.railway.app/api"
+  // ⚠️ apna backend URL yaha daalo
 });
 
 export default API;
