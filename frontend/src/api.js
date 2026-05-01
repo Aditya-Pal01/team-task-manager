4618df42c219fdd39d0a1ba7aec9edff2ece8395
Aditya Pal <2k22.csds.2212974@gmail.com>
2026-05-02 01:36:21 +0530

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://considerate-purpose-production-2082.up.railway.app/api"
+  baseURL: "https://team-task-manager-production-f529.up.railway.app/api"
 });
 
 // 🔥 TOKEN AUTO ADD
